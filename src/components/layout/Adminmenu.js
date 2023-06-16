0c654047e8dcd52c1885f516b0f48461dcbe08d4
YogitaModi <yogitamodi00@gmail.com>
2023-06-16 23:47:21 +0530

    created menu for admin dashboard

--- src/components/layout/Adminmenu.js
+++ src/components/layout/Adminmenu.js
@@ -3,29 +3,31 @@ import { NavLink } from "react-router-dom";
 
 const Adminmenu = () => {
   return (
-    <div className="Text-center">
-      <div className="list-group">
-        <h4>Admin-Panel</h4>
-        <NavLink
-          to="/dashboard/admin/create-category"
-          className="list-group-item list-group-item-action"
-        >
-          Create Category
-        </NavLink>
-        <NavLink
-          to="/dashboard/admin/create-product#"
-          className="list-group-item list-group-item-action"
-        >
-          Create product
-        </NavLink>
-        <NavLink
-          to="/dashboard/admin/users"
-          className="list-group-item list-group-item-action"
-        >
-          Users
-        </NavLink>
+    <>
+      <div className="Text-center">
+        <div className="list-group">
+          <h4>Admin-Panel</h4>
+          <NavLink
+            to="/dashboard/admin/create-category"
+            className="list-group-item list-group-item-action"
+          >
+            Create Category
+          </NavLink>
+          <NavLink
+            to="/dashboard/admin/create-product"
+            className="list-group-item list-group-item-action"
+          >
+            Create product
+          </NavLink>
+          <NavLink
+            to="/dashboard/admin/users"
+            className="list-group-item list-group-item-action"
+          >
+            Users
+          </NavLink>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
