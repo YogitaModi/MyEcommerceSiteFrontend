e2a1d0d977be26b3d34ec331ef07bef27552cba3
YogitaModi <yogitamodi00@gmail.com>
2023-06-16 23:48:08 +0530

    created admin dashboard

--- src/pages/Admin/Admindashboard.js
+++ src/pages/Admin/Admindashboard.js
@@ -1,7 +1,9 @@
 import React from "react";
 import Layout from "../../components/layout/Layout";
 import Adminmenu from "../../components/layout/Adminmenu";
+import { useAuth } from "../../context/authContext";
 const Admindashboard = () => {
+  const [auth] = useAuth();
   return (
     <Layout>
       <div className="container-fluid m-3 p-3">
@@ -9,7 +11,14 @@ const Admindashboard = () => {
           <div className="col-md-3">
             <Adminmenu />
           </div>
-          <div className="col-md-9">content</div>
+          <div className="col-md-9">
+            <div className="card w-75 p-3">
+              <h4>Admin name : {auth?.user?.name}</h4>
+              <h4>Admin email : {auth?.user?.email}</h4>
+              <h4>Admin contact : {auth?.user?.phone}</h4>
+              <h4>Admin address : {auth?.user?.address}</h4>
+            </div>
+          </div>
         </div>
       </div>
     </Layout>
