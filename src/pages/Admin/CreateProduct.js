4952d1569972ea5353a8009f80ff4deac8ff7627
YogitaModi <yogitamodi00@gmail.com>
2023-07-08 18:40:43 +0530

    removed unused modules

--- src/pages/Admin/CreateProduct.js
+++ src/pages/Admin/CreateProduct.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import Layout from "../../components/layout/Layout";
 import Adminmenu from "../../components/layout/Adminmenu";
 import axios from "axios";
