0fc22caca0ab9b08a7026d5c266967ee188fb737
YogitaModi <yogitamodi00@gmail.com>
2023-07-08 18:15:32 +0530

    edited css

--- src/pages/Categories.js
+++ src/pages/Categories.js
@@ -2,7 +2,6 @@ import React from "react";
 import Layout from "../components/layout/Layout";
 import useCategory from "../hooks/useCategory";
 import { Link } from "react-router-dom";
-import "../style/categories.css";
 
 const Categories = () => {
   const categories = useCategory();
