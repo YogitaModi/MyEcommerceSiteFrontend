f72bd7c4aae67d0eab3909c2161f6af1bb328f1e
YogitaModi <yogitamodi00@gmail.com>
2023-07-12 22:51:42 +0530

    corrected authentication ques in forgot password

--- src/pages/Auth/Forgotpassword.js
+++ src/pages/Auth/Forgotpassword.js
@@ -60,7 +60,7 @@ const Forgotpassword = () => {
 
           <div className="mb-3">
             <label htmlFor="answer" className="form-label">
-              What is the name of your primary school?
+              Which is your favourite Chocolate?
             </label>
             <input
               type="text"
